91fe354a11b6ba97bf021079a5b1a837fbdb303a
Andy <43708848+andriilive@users.noreply.github.com>
2021-06-04 16:55:19 +0200

    Testing Translatable - Not translatable fields

--- web/app/themes/sample-theme/acf-fields/test-wpml-translatable.php
+++ web/app/themes/sample-theme/acf-fields/test-wpml-translatable.php
@@ -3,7 +3,7 @@
 if (function_exists('acf_add_local_field_group')) :
     acf_add_local_field_group(array(
         'key' => 'group_60b8c79105d15',
-        'title' => 'Test',
+        'title' => 'ACF: Translate',
         'fields' => array(
             array(
                 'key' => 'field_60b8c7936d4b2',
---
+++ web/app/themes/sample-theme/template-nottranslate.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Fields (NOT TRANSLATE)
+ *
+ */
+
+get_header(); ?>
+
+<?= get_template_directory() ?>
+
+<pre><?php print_r(get_fields()) ?></pre>
+
+<?php get_footer();
